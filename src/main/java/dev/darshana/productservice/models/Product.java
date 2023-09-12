f752e1194f7617b05e5121688b92c60d0a5d4cc5
Darshana <darshana.navgale@gmail.com>
2023-09-12 16:59:46 +0530

    Added JPA code and UUID generator

--- src/main/java/dev/darshana/productservice/models/Product.java
+++ src/main/java/dev/darshana/productservice/models/Product.java
@@ -1,12 +1,16 @@
 package dev.darshana.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Setter;
 
+@Entity
 @Setter
 public class Product extends BaseModel{
     private String title;
     private String description;
     private String image;
+    @ManyToOne
     private Category category;
     private double price;
 }
